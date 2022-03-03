877ba358240c28228fbbd263e8ab114584bb1709
JustinCurtisKoch <justincurtiskoch@gmail.com>
2022-03-03 08:07:44 -0700

    delete

--- providers.tf
+++ providers.tf
@@ -11,4 +11,8 @@ terraform {
 #Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+    subscription_id = "8d12a595-ad13-41df-bf17-0a5b9ad7aa90"
+  client_id       = "bba2906c-68e8-408d-8061-6358ca4d3c37"
+  client_secret   = "gA57Q~nNvfxTjafqOohxnnoY.zuFZhDHPpckl"
+  tenant_id       = "33da9f3f-4c1a-4640-8ce1-3f63024aea1d"
 }
